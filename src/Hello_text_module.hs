8b73130cbd1e12055ade404336dc9951c48a9631
Jaroslaw Wiosna <jaroslaw.wiosna.github@gmail.com>
2019-07-17 15:01:54 +0200

    Read file content and print it to the console
    Closes #6

--- src/Hello_text_module.hs
+++ src/Hello_text_module.hs
@@ -5,7 +5,6 @@ module Hello_text_module where
 import qualified Data.Text as T
 import qualified Data.Text.IO as T (readFile)
 
-f = do
-    fileContent <- T.readFile "./hello.txt"
-    return fileContent
+f :: IO T.Text
+f = T.readFile "./hello.txt"
 
--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,11 @@
 import Hello_text_module
 
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 main = do
     putStrLn "Hello, World!"
     content_of_my_file <- f
-    putStrLn content_of_my_file
+    TIO.putStrLn content_of_my_file
+    TIO.putStrLn "END"
 
