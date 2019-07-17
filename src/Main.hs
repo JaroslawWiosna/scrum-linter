920bec3dc4145932b20355cd38f399c5a4fd6fd4
Jaroslaw Wiosna <jaroslaw.wiosna.github@gmail.com>
2019-07-17 15:01:54 +0200

    Recognize `--prompt` option, but
    ... throw **undefined** for now

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,8 @@
 
 import Hello_text_module
 
+import System.Environment
+import System.Exit
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
@@ -9,5 +11,16 @@ main = do
     putStrLn "Hello, World!"
     content_of_my_file <- f
     TIO.putStrLn content_of_my_file
+    getArgs >>= parse >>= putStr
     TIO.putStrLn "END"
 
+
+parse ["-h"]       = usage   >> exit
+parse ["-v"]       = version >> exit
+parse ["--prompt"] = undefined
+parse _            = die
+
+usage   = putStrLn "help"
+version = putStrLn "scrum-linter version 0.1.0.0"
+exit    = exitSuccess
+die     = exitWith (ExitFailure 1)
